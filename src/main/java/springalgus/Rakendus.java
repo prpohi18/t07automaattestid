b93897edd08a032efe09c5f6a5e2612d8b4dfbb2
Sergei Frolov <serfro@tlu.ee>
2018-03-08 11:28:09 +0200

    klassitoo

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -1,6 +1,5 @@
 package springalgus;
 
-import java.util.stream.IntStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping; 
@@ -20,21 +19,23 @@ public class Rakendus {
     
     @RequestMapping("/korrutus")
     int korrutamine(int arv1, int arv2){
-       return arv1*arv2; 
-    }
-    @RequestMapping("/summa")
-    double summa(String arvud){ 
-        double sum = 0;
-        String[] temp = arvud.split(",");
-        for (int i = 0; i < temp.length; i++) {
-            sum += Double.parseDouble(temp[i].trim());
-        }
-        return sum;
-        
-        java.util.Arrays.stream(arvud.split(",")).map(s -> s.trim());
+       return arv1*arv2;
     }
     //http://localhost:8080/korrutus?arv1=3&arv2=5
  
+    @RequestMapping("/liidaArvud")
+    public int arvudeLiitmine(String sisu){
+        String[] m=sisu.split(",");
+        int summa=0; 
+        for(int i=0; i<m.length; i++){
+           summa+=Integer.parseInt(m[i].trim());
+        }
+        return summa;
+        
+       // return java.util.Arrays.stream(sisu.split(",")).
+       //          mapToInt(s -> Integer.parseInt(s.trim())).sum(); 
+    }
+    //http://localhost:8080/liidaArvud?sisu=1,10,200
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
