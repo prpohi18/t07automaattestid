db76afebf4fb979cde5016f6deab905399cbe890
Sergei Frolov <serfro@tlu.ee>
2018-03-08 09:53:19 +0200

    klassitoo

---
+++ src/main/java/springalgus/Rakendus.java
@@ -0,0 +1,30 @@
+package springalgus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Rakendus {
+	@RequestMapping("/algus")
+    String tervitusfunktsioon() {
+        return "Ahoi!!";
+    }
+    
+    @RequestMapping("/tervitus")
+    String tervitus2(String eesnimi){
+        return "Tere, "+eesnimi;
+    }
+    
+    @RequestMapping("/korrutus")
+    int korrutamine(int arv1, int arv2){
+       return arv1*arv2;
+    }
+    //http://localhost:8080/korrutus?arv1=3&arv2=5
+ 
+    public static void main(String[] args) {
+		//System.getProperties().put("server.port", 40305);
+        SpringApplication.run(Rakendus.class, args);
+    }
+}
