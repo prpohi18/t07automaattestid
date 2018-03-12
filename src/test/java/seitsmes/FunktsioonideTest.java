161a5ac7825cddbab09f9542fd17b005e20a59d2
Jaagup Kippar <jaagup@tlu.ee>
2018-03-12 10:27:12 +0200

    Update FunktsioonideTest.java

--- src/test/java/seitsmes/FunktsioonideTest.java
+++ src/test/java/seitsmes/FunktsioonideTest.java
@@ -7,4 +7,9 @@ public class FunktsioonideTest{
     public void korrutuseTest(){
        assertEquals(6, Funktsioonid.korruta(3, 2));
     }
+    
+    @Test
+    public void pindala1(){
+       assertEquals(6.0, Funktsioonid.kolmnurgaPindala(3, 4, 5), 0.01);
+    }
 }
