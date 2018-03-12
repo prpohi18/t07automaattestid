ce773966207ded3ab95f2e40be31e85769c6b818
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-03-12 11:27:24 +0200

    muutused

--- src/main/java/seitsmes/Alustus.java
+++ src/main/java/seitsmes/Alustus.java
@@ -2,5 +2,9 @@ package seitsmes;
 public class Alustus{
   public static void main(String[] args){
     System.out.println("algus");
+	System.out.println(Funktsioonid.kolmnurgaPindala(3, 4, 5));
   }
 }
+
+//mvn package
+//java -cp target/testid-1.jar seitsmes.Alustus
