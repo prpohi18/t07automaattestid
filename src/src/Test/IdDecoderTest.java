cff5d0729a1f581a41c087e6ccc1777cb568311a
Mihkel Mägi <mihkel26@tlu.ee>
2018-03-15 23:09:03 +0200

    Created length logic

--- src/src/Test/IdDecoderTest.java
+++ src/src/Test/IdDecoderTest.java
@@ -1,29 +1,33 @@
 import IdDecoder.IdDecoder;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class IdDecoderTest {
-    private IdDecoder IdDecoder;
+    private IdDecoder IdDecoderCorrect;
+    private IdDecoder IdDecoderInvalidShorter;
+    private IdDecoder IdDecoderInvalidLonger;
 
     @Before
     public void setUp() throws Exception {
-        IdDecoder = new IdDecoder("39603265713");
+        IdDecoderCorrect = new IdDecoder("39603265713");
+        IdDecoderInvalidShorter = new IdDecoder("678454");
+        IdDecoderInvalidLonger = new IdDecoder("45609674359837645987357");
     }
 
     @Test
     public void testIdCodeLengthFalseLonger(){
-        assertEquals(IdDecoder.checklength(), false);
+        assertEquals(IdDecoderInvalidLonger.checklength(), false);
     }
 
     @Test
     public void testIdCodeLengthFalseShorter(){
-        assertEquals(IdDecoder.checklength(), false);
+        assertEquals(IdDecoderInvalidShorter.checklength(), false);
     }
 
     @Test
     public void testIdCodeLengthTrue(){
-        assertEquals(IdDecoder.checklength(), true);
+        assertEquals(IdDecoderCorrect.checklength(), true);
     }
 
 
