47c19d9990c4d8aae8aaa3b6ae8b5863ea543645
Sander <Sander@Sander.lan>
2018-03-21 20:54:00 +0200

    Before setup method

---
+++ automaattestid/src/main/java/Kolmnurk.java
@@ -0,0 +1,13 @@
+public class Kolmnurk {
+    
+    int a, b, c;
+    
+    public Kolmnurk(int kylg1, int kylg2, int kylg3) {
+        a = kylg1;
+        b = kylg2;
+        c = kylg3;
+    }
+    
+    
+    
+}
---
+++ automaattestid/src/test/java/Test1.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class Test1 {
+    
+    public Test1() {
+    }
+    
+    Kolmnurk k;
+    @Before
+    public void setUp() {
+        k = new Kolmnurk(2, 5, 6);
+    }
+    
+    
+}
