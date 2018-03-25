8f01d492ddda3c88e89db3b34860739c0a918b18
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-03-26 01:46:02 +0300

    lisatud test

---
+++ src/test/java/seitsmes/FunktsioonTest.java
@@ -0,0 +1,10 @@
+package test.java.seitsmes;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class FunktsioonTest {
+    @Test
+    public void distanceTest(){
+        assertEquals(4.0, Funktsioonid.distance(4.0, 3.0, 8.0, 3.0));
+    }
+}
