1a065e50589d27fcd58c70c180ffa87fb89d1a28
Kert Tamm <31847373+K3RTTAMM@users.noreply.github.com>
2018-03-26 08:41:12 +0300

    added test example info

--- src/test/java/seitsmes/FunktsioonideTest.java
+++ src/test/java/seitsmes/FunktsioonideTest.java
@@ -9,3 +9,18 @@ public class FunktsioonideTest{
        assertEquals(27.75, Funktsioonid.kmiarvutus(95, 1.85), 0.1);
     }	
 }
+
+/*
+Results :
+
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0
+
+[INFO] ------------------------------------------------------------------------
+[INFO] BUILD SUCCESS
+[INFO] ------------------------------------------------------------------------
+[INFO] Total time: 3.239s
+[INFO] Finished at: Mon Mar 26 08:40:35 EEST 2018
+[INFO] Final Memory: 17M/243M
+[INFO] ------------------------------------------------------------------------
+[tammkert@greeny t07automaattestid]$
+*/
