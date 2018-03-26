6e0063f58df30afdd8d91714b828ca085c4443bc
Kertu Kipper <phkel@users.noreply.github.com>
2018-03-26 13:59:57 +0300

    Add pyramid area

---
+++ src/main/java/pyramid/Pyramid.java
@@ -0,0 +1,7 @@
+package pyramid;
+
+public class Pyramid {
+  public static int areaCheck(int k, int a, int n){
+    return k * a * n / 2;
+  }
+}
---
+++ src/test/java/pyramid/Testid1.java
@@ -0,0 +1,12 @@
+package pyramid;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Testid1{
+
+  @Test
+  public void test(){
+    assertEquals(3, 1+2);
+  }
+}
