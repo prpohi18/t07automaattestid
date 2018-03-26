de2bcbffeb0362d84e9224e2b52ac4d29ac52279
Kertu Kipper <phkel@users.noreply.github.com>
2018-03-26 14:02:21 +0300

    Add volume

--- src/main/java/pyramid/Pyramid.java
+++ src/main/java/pyramid/Pyramid.java
@@ -4,4 +4,7 @@ public class Pyramid {
   public static int areaCheck(int k, int a, int n){
     return k * a * n / 2;
   }
+  public static int volumeCheck(int k, int a, int n, int h){
+    return k * a * n * h / 6;
+  }
 }
