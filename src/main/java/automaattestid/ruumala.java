ad591643724223d0ffbabcd1509a7ddd713bb472
Kalmer Roopa <kalmerr@tlu.ee>
2018-03-26 08:42:35 +0300

    Lisatud korrapärase nelikurkse püramiidi ruumala valem

---
+++ src/main/java/automaattestid/ruumala.java
@@ -0,0 +1,9 @@
+package automaattestid;
+import java.lang.Math;
+
+public class ruumala {
+	public static double nnpyraRuumala(double pserv, double korgus){
+		double V = 1/3 * Math.pow(pserv, 2) * korgus;
+		return V;
+	} 
+}
