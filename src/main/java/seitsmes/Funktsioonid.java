d801e651123f307158beb7175d1fd49ff48b5b9a
j71m <23724760+J71m@users.noreply.github.com>
2018-03-26 08:27:10 +0300

    algus

---
+++ src/main/java/seitsmes/Funktsioonid.java
@@ -0,0 +1,12 @@
+package seitsmes;
+public class Funktsioonid{
+   public static int korruta(int arv1, int arv2){
+      return arv1*arv2;
+   }
+   public static double kolmnurgaPindala(double kylg1, double kylg2, double kylg3){
+       return 6;
+   }
+   public static double poolYmbermootu(double kylg1, double kylg2, double kylg3){
+    return 12;
+   }
+}
---
+++ src/test/java/seitsmes/FunktsioonideTest.java
@@ -0,0 +1,21 @@
+package seitsmes;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class FunktsioonideTest{
+    @Test
+    public void korrutuseTest(){
+       assertEquals(6, Funktsioonid.korruta(3, 2));
+    }
+    
+    @Test
+    public void pindala1(){
+       assertEquals(6.0, Funktsioonid.kolmnurgaPindala(3, 4, 5), 0.01);
+       //assertEquals(24.0, Funktsioonid.kolmnurgaPindala(6, 8, 10), 0.01);
+       //assertEquals(0.43, Funktsioonid.kolmnurgaPindala(1, 1, 1), 0.01);
+    }
+    @Test
+    public void ymbermooduTest1(){
+       assertEquals(12, Funktsioonid.poolYmbermootu(6, 8, 10), 0.01);
+    }
+}
