3b388410ce8c9101d62ec2e45e0d10bbcc27b2f1
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-03-26 08:52:31 +0300

    lisatud ringi funktsioonid

--- src/main/java/seitsmes/Funktsioonid.java
+++ src/main/java/seitsmes/Funktsioonid.java
@@ -4,13 +4,13 @@ public class Funktsioonid{
         double d = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
         return d;
     }
-	public static double xDistance(double x1, double y1, double x2, double y2){
-		double c = distance(0.0, 0.0, x1, y1);
-		return c;
+	public static double area(double R){
+		double area = 3.14 * (R * R);
+		return area;
 	}
-	public static double yDistance(double x1, double y1, double x2, double y2){
-		double c = distance(0.0, 0.0, x2, y2);
-		return c;
+	public static double circumference(double R){
+		double circumference = 2 * 3.14 * R;
+		return circumference;
 	}
 	
 }
