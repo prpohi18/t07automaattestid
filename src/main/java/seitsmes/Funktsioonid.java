61785649bc6a8c293ebe48769e515bd68ff7fb65
Julika Maiste <julimai@users.noreply.github.com>
2018-03-26 09:48:03 +0300

    automaattestid

---
+++ src/main/java/seitsmes/Funktsioonid.java
@@ -0,0 +1,10 @@
+package seitsmes;
+public class Funktsioonid{
+   public static int keskmine(int arv1, int arv2){
+      return (arv1+arv2)/2;
+   }
+   public static int korrutus(int arv1, int arv2){
+      return arv1*arv2;
+   }
+  
+}
