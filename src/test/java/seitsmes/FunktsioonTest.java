6122fe680459554bc1f34ff04fa2445a00912771
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-03-26 08:23:35 +0300

    test tootab

--- src/test/java/seitsmes/FunktsioonTest.java
+++ src/test/java/seitsmes/FunktsioonTest.java
@@ -1,10 +1,10 @@
-package test.java.seitsmes;
+package seitsmes;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class FunktsioonTest {
     @Test
     public void distanceTest(){
-        assertEquals(4.0, Funktsioonid.distance(4.0, 3.0, 8.0, 3.0));
+        assertEquals(4.0, Funktsioonid.distance(4.0, 3.0, 8.0, 3.0), 0.01);
     }
 }
