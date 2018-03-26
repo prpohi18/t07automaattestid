c1aed7f50bbfc404ef3cf4c4772931196e227ecc
Kalmer Roopa <kalmerr@tlu.ee>
2018-03-26 09:06:45 +0300

    Lisatud 2 testi juurde

--- src/test/java/automaattestid/ruumalaTest.java
+++ src/test/java/automaattestid/ruumalaTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 public class ruumalaTest{
 	@Test
 	public void ruumala(){
-		assertEquals(100.00, ruumala.nnpyraRuumala(5.0, 12.0), 0.01); 
+		assertEquals(100.00, ruumala.nnpyraRuumala(5.0, 12.0), 0.01);
+		assertEquals(328.56, ruumala.nnpyraRuumala(7.4, 18.0), 0.01);
+		assertEquals(211.96, ruumala.nnpyraRuumala(9.6, 6.9), 0.01);		
 	}
 }
