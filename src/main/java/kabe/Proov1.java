186b35b97696126f0fb66340fed88cae7b7631ef
Grete Ojavere <GreteO@users.noreply.github.com>
2018-03-27 09:19:40 +0300

    Pindala

---
+++ src/main/java/kabe/Proov1.java
@@ -0,0 +1,11 @@
+package kabe;
+
+public class Proov1{
+	public static void main(String[] arg){
+	KabeLaud laud=new GreteKabeLaud();
+	laud.algseis();
+	System.out.println(laud);
+	laud.paiguta("B4", 'v');
+	System.out.println(laud);
+	}
+}
