ca9b1888807aceb033084c0018226c2ab5692ff7
Kertu Mikk <kertumik@tlu.ee>
2018-03-27 20:25:30 +0300

    risttahuka ruumalatest

---
+++ Kodunetoo7/src/main/java/com/mycompany/kodunetoo7/Ruumala.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.kodunetoo7;
+
+
+
+public class Ruumala {
+    public static double Ruumala(int a,int b, int h){
+        return a * b * h;
+    }
+    
+
+    public static Object tulemus(int a,int b, int h) {
+        double V = Ruumala(2,3,5);
+                return V;
+    }
+        
+}
