279515e803e59a0b5dfc93b36e8d97486a6b741e
Kaur K <kaurk@users.noreply.github.com>
2018-03-27 09:24:10 +0300

    Fine tuning

--- src/main/java/homework7/mainClass.java
+++ src/main/java/homework7/mainClass.java
@@ -1,7 +1,7 @@
 package homework7;
 
 public class mainClass {
-    public static double averageNR (double a, double b, double c){
+    public static double averageNr (double a, double b, double c){
         return (a+b+c)/3;
     }           
             
--- src/test/java/homework7/testsUnit.java
+++ src/test/java/homework7/testsUnit.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class testsUnit {
     @Test
     public void testUnit() {
-        assertEquals(6.66, mainClass.averageNR(4,7,9), 0.1);
+        assertEquals(6.66, mainClass.averageNr(4,7,9), 0.1); //third is delta which is a must with floating numbers (basically it's max allowed difference)
         
     }
 }
