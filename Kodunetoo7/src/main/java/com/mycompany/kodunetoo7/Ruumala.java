29ca6d859b4c568928ffef701fe575530682138c
Kertu Mikk <kertumik@tlu.ee>
2018-03-27 21:06:37 +0300

    erinevad risttahukad

--- Kodunetoo7/src/main/java/com/mycompany/kodunetoo7/Ruumala.java
+++ Kodunetoo7/src/main/java/com/mycompany/kodunetoo7/Ruumala.java
@@ -4,14 +4,25 @@ package com.mycompany.kodunetoo7;
 
 
 public class Ruumala {
-    public static double Ruumala(int a,int b, int h){
+    public int a;
+    public int b;
+    public int h;
+    public Ruumala(int ka, int kb,int kh) {
+        a=ka;
+        b=kb;
+        h=kh;
+    }
+    
+    public static double Ruumala1(int a,int b, int h){
         return a * b * h;
     }
     
 
     public static Object tulemus(int a,int b, int h) {
-        double V = Ruumala(2,3,5);
+        double V = Ruumala1(a,b,h);
                 return V;
     }
+
+    
         
 }
--- Kodunetoo7/src/test/java/RuumalaTest.java
+++ Kodunetoo7/src/test/java/RuumalaTest.java
@@ -7,14 +7,17 @@ public class RuumalaTest {
     public void Test() {
         
     }
+    @Before
+    public void setUp(){
+        Ruumala r1 = new Ruumala(3,4,5);
+        Ruumala r2 = new Ruumala(4,5,6);
+       
+    }    
     
     @Test
-    public void test1() {
-        assertEquals(2,3,4);
-    }
-    @Test
     public void test2(){
-        assertEquals(30.0,Ruumala.tulemus(3,4,5));
+        assertEquals(60.0,Ruumala.tulemus(3,4,5));
     }
+    
     
 }
