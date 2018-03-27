5c91ae47e8da188871baf1a73f043a95b18ad1ae
Rando Aljaste <31949050+randoaljaste@users.noreply.github.com>
2018-03-27 08:29:12 +0300

    kodutoo7

---
+++ src/main/java/kaheksas/Funktsioonid.java
@@ -0,0 +1,11 @@
+package kaheksas;
+public class Funktsioonid{
+   public static double pyth(int a, int b){
+      return Math.sqrt((a*a) + (b*b));
+   }
+   
+   public static double ymbermoot(int a, int b, double c){
+      c = pyth(3, 4);
+	  return a+b+c;
+   }
+}
