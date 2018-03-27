d7eaea2119f97e339fdff78d843f35767980220c
Liisa Jakovleva <liisajako@users.noreply.github.com>
2018-03-27 09:21:10 +0300

    kodutöö

---
+++ src/main/java/seitsmes/Arvutused.java
@@ -0,0 +1,10 @@
+package seitsmes;
+public class Arvutused{
+	public static int pindala(int a, int b){
+		return a*b;
+	}
+	
+	public static int umbermoot(int pikkus, int laius){
+		return 2*(pikkus+laius);
+	}
+}
