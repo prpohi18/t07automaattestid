42932b89ce4215abca07bf01d6adceb9294ebe4a
Kertu Mikk <kertumik@tlu.ee>
2018-03-28 13:52:38 +0300

    kõik toimib

--- Kodunetoo7/src/test/java/RuumalaTest.java
+++ Kodunetoo7/src/test/java/RuumalaTest.java
@@ -10,26 +10,30 @@ public class RuumalaTest {
     }
     protected Ruumala r1;
     protected Ruumala r2;
+    double tulemus2;
+    double tulemus1;
     @Before
     public void setUp(){
         r1 = new Ruumala(3,4,5);
+        tulemus1 = r1.tulemus();
         r2 = new Ruumala(4,5,6); 
+        tulemus2 = r2.tulemus();
     }    
     
     @Test
     public void test1(){
-       assertTrue("esimese ruumala on 60",(r1.tulemus() == 60.0));
+       assertTrue("esimese ruumala ei ole 60",(tulemus1 == 60.0));
         
     }
     @Test
     public void test2(){
-        assertTrue("teise ruumala on 120",(r2.tulemus() == 120.0));
+        assertTrue("teise ruumala ei ole 120",(tulemus2 == 120.0));
        
     }
     
     @Test
     public void test3(){
-        assertTrue("esimene väiksem või võrdne teisega",(r1.tulemus() <= r2.tulemus()));
+        assertTrue("esimene ei ole väiksem või võrdne teisega",(r1.tulemus() <= r2.tulemus()));
     }
     
 }
