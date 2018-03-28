c84c0c171493a0462f3e2c4777797a01571229e7
Tauri Taevik <31793402+ttaevik@users.noreply.github.com>
2018-03-28 10:28:12 +0300

    automaattestid

---
+++ automaattestid/src/main/java/ee/tlu/ttaevik/automaattestid/Functions.java
@@ -0,0 +1,21 @@
+
+package ee.tlu.ttaevik.automaattestid;
+
+
+public class Functions {
+    public static double ymbermoot(double arv1, double arv2){
+		return 2*(arv1+arv2);
+   }
+    
+    
+   public static double korrutus(double arv1, double arv2){
+		return arv1*arv2;
+
+   }
+   
+   public static String tervitus(String eesnimi){
+		return "Tere, "+eesnimi;
+
+   }
+    
+}
