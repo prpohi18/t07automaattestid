7b772c9a5e26194c282b8ed23c866bd54ac206c1
if17 <if17@A406-25.INFORMAATIKA>
2018-03-29 08:50:24 +0300

    Kodutoo7

---
+++ src/main/java/ee/tlu/kodasma/automaattestid/Kolmnurk.java
@@ -0,0 +1,12 @@
+package ee.tlu.kodasma.automaattestid;
+
+public class Kolmnurk {
+    public static double pindala (int a, int h) {
+        return (a*h)/2;
+    }
+    
+    public static double vastus (int a, int h, double s) {
+        s = pindala(2, 3);
+        return s;
+    }
+}
