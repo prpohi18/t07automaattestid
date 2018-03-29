64edb9dd796cc45fa58ff34a96ccf9b71a97df53
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-03-29 08:48:19 +0300

    Update funktsioon.java

--- src/main/java/ee/tlu/jaagup/automaattestid/funktsioon.java
+++ src/main/java/ee/tlu/jaagup/automaattestid/funktsioon.java
@@ -15,4 +15,19 @@ public class funktsioon {
                 return vastus * 1024;
                 
     }
-}
+}/*
+Running ee.tlu.jaagup.automaattestid.AutomaatTest
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.097 sec
+
+Results :
+
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+
+------------------------------------------------------------------------
+BUILD SUCCESS
+------------------------------------------------------------------------
+Total time: 1.309s
+Finished at: Thu Mar 29 08:42:25 EEST 2018
+Final Memory: 5M/123M
+------------------------------------------------------------------------
+*/
