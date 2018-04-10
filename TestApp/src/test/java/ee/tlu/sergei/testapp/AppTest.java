18131069ef3dd29fe1bb964e950c01e078ce86fd
Sergei Frolov <serfro@tlu.ee>
2018-04-10 13:19:25 +0300

    tehtud

--- TestApp/src/test/java/ee/tlu/sergei/testapp/AppTest.java
+++ TestApp/src/test/java/ee/tlu/sergei/testapp/AppTest.java
@@ -20,3 +20,13 @@ public class AppTest {
     }
     
 }
+/*
+Running ee.tlu.sergei.testapp.AppTest
+ArraySum
+KronKesk2
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.074 sec
+
+Results :
+
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0
+*/
