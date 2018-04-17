e1c3a8ec827d24bb752265ea34f199341bc96969
Egert Klaamas <31847333+egert112@users.noreply.github.com>
2018-04-17 14:19:54 +0300

    Nelinurk Ja ArvudeKeskmine Automaat test

---
+++ AutomaatTest/src/main/java/Automaat/Funktsioonid.java
@@ -0,0 +1,10 @@
+package Automaat;
+public class Funktsioonid{
+   public static int keskmine(int arv1, int arv2){
+      return (arv1+arv2)/2;
+   }
+   public static int Nelinurk(int arv1, int arv2){
+      return arv1*arv2;
+   }
+  
+}
