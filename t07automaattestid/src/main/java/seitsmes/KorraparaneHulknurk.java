b14c47c8dbc5ebb38bc18d55c6eb9cb4d9bd784f
Sten Markus Laht <31847424+stenmarkus@users.noreply.github.com>
2018-05-06 23:18:49 +0300

    kodutöö7

---
+++ t07automaattestid/src/main/java/seitsmes/KorraparaneHulknurk.java
@@ -0,0 +1,7 @@
+package t07automaattestid;
+public class KorraparaneHulknurk{
+	public static int hulknurk (int kylgedearv, int kyljepikkus, int raadius){
+		int pindala = (kylgedearv*kyljepikkus*raadius)/2;
+		return pindala;
+	}
+}
