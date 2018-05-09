da910efc865d6feaea2878717d9bd3b517ebf186
Krister Riska <31847374+kristerriska@users.noreply.github.com>
2018-05-09 10:34:53 +0300

    automaattestid

---
+++ src/test/java/kaheksas/FunktsioonideTest.java
@@ -0,0 +1,21 @@
+package kaheksas;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class FunktsioonideTest{
+    @Test
+    public void Test1(){
+       assertEquals(5, Funktsioonid.pyth(3, 4), 0.01);
+    }
+    
+    @Test
+    public void Test2(){
+       assertEquals(12.0, Funktsioonid.ymbermoot(3, 4, 5), 0.01);
+    }
+	
+    @Test
+    public void Test3(){
+       assertEquals(6.0, Funktsioonid.pindala(3, 4), 0.01);
+    }
+   
+}
