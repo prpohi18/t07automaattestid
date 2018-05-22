713b093e0ebcc0cbf14cf0556cf03469e8006a19
elinorroosalu <elinorroosalu@aljos-mbp.lan>
2018-05-22 22:55:18 +0300

    Ruudu pindala, ümbermõõdu ja ruumala test

---
+++ automaattestid/src/main/java/ee/tlu/elinorr/automaattestid/Ruut.java
@@ -0,0 +1,14 @@
+package ee.tlu.elinorr.automaattestid;
+public class Ruut {
+    
+    public static int Umbermoot(int kulg){
+        return kulg*4;
+    }
+    public static int Pindala(int kulg){
+        return kulg*kulg;
+    }
+    
+    public static int Ruumala(int kulg){
+        return kulg*kulg*kulg;
+    }
+}
